1906c7ec20473185a16a717c73363689ab5b1657
Edward Suwirya <edward.suwirya@btpn.com>
2017-08-23 19:56:24 +0700

    Box Registration prep 1

--- src/app/box-registration/component/form/box-registration.component.ts
+++ src/app/box-registration/component/form/box-registration.component.ts
@@ -1,9 +1,11 @@
 import {Component, OnInit, AfterViewInit, ViewChild} from "@angular/core";
 import {DatePickerComponent} from "../../../shared/component/date-picker/date-picker.component";
 import {BoxRegistration} from "../../model/box-registration";
+import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 /**
  * Created by 15050978 on 8/23/2017.
  */
+declare let _:any;
 @Component({
     selector: 'arms-box-registration',
     templateUrl: './box-registration.component.html',
@@ -11,21 +13,70 @@ import {BoxRegistration} from "../../model/box-registration";
 })
 export class BoxRegistrationComponent implements OnInit,AfterViewInit {
     @ViewChild('startValidDate')
-    startValidDate: DatePickerComponent;
+    startValidDate:DatePickerComponent;
 
     @ViewChild('endValidDate')
-    endValidDate: DatePickerComponent;
+    endValidDate:DatePickerComponent;
 
-    listBoxRegistrations: BoxRegistration[] = [
-        new BoxRegistration('123', 'Cab. Jakarta Pusat', 'Keterangan', 'Kecil', 'Internal', 'Jakarta', '1', '2', '10 Aug 2017', '11 Aug 2018')
-    ];
+    boxRegistrationFormGroup:FormGroup;
 
-    ngOnInit(): void {
+    listBoxRegistrations:BoxRegistration[];
+
+    constructor(private formBuilder:FormBuilder) {
     }
 
-    ngAfterViewInit(): void {
-        this.startValidDate.setPickerDate(null);
-        this.endValidDate.setPickerDate(null);
+    ngOnInit():void {
+        this.boxRegistrationFormGroup = this.formBuilder.group({
+            idBox: ['', Validators.required],
+            workingUnit: ['', Validators.required],
+            boxDescription: ['', Validators.required],
+            boxSize: ['S', Validators.required],
+            depositType: ['I', Validators.required],
+            depositLocation: ['', Validators.required],
+            depositRoom: ['', Validators.required],
+            depositShelf: ['', Validators.required],
+            startValidDate: ['', Validators.required],
+            endValidDate: ['', Validators.required],
+        });
     }
 
+    ngAfterViewInit():void {
+        this.startValidDate.setPickerDate();
+        this.endValidDate.setPickerDate();
+    }
+
+    doGetStartDate(event) {
+        this.boxRegistrationFormGroup.controls['startValidDate'].setValue(event);
+    }
+
+    doGetEndDate(event) {
+        this.boxRegistrationFormGroup.controls['endValidDate'].setValue(event);
+    }
+
+    doSave() {
+        let formVal = this.boxRegistrationFormGroup.getRawValue();
+        console.log(formVal);
+        if (!this.listBoxRegistrations) {
+            this.listBoxRegistrations = [];
+        }
+        this.listBoxRegistrations.push(new BoxRegistration(formVal.idBox, formVal.workingUnit, formVal.boxDescription, formVal.boxSize, formVal.depositType, formVal.depositLocation, formVal.depositRoom, formVal.depositShelf, formVal.startValidDate, formVal.endValidDate));
+    }
+
+    doReset() {
+        this.boxRegistrationFormGroup.reset();
+        this.boxRegistrationFormGroup.controls['boxSize'].setValue('S');
+        this.boxRegistrationFormGroup.controls['depositType'].setValue('I');
+    }
+
+    doView(box:BoxRegistration) {
+    }
+
+    doUpdate(box:BoxRegistration) {
+    }
+
+    doDelete(box:BoxRegistration) {
+        let boxIndex = this.listBoxRegistrations.indexOf(box);
+        this.listBoxRegistrations.splice(boxIndex, 1);
+
+    }
 }
